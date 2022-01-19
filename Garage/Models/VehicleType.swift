50788ee8587a5f7e6c7c045798e1b08072f94e7a
Arian Mohajer <amohajer45@gmail.com>
2022-01-19 14:16:59 -0700

    Basic UI for garage and detail views implemented

---
+++ Garage/Models/VehicleType.swift
@@ -0,0 +1,15 @@
+//
+//  VehicleType.swift
+//  Garage
+//
+//  Created by Arian Mohajer on 1/19/22.
+//
+
+import Foundation
+
+enum VehicleType {
+    case sedan
+    case truck
+    case coupe
+    case wagon
+}
